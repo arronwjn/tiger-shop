fb13a95c190269a9e479011eff3d79f2b2ada1b3
wangjiannan <904089747@qq.com>
2017-04-02 21:28:24 +0800

    second

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import  App from './App';
-import {Provider} from 'react-redux'
-import store from './store'
-import tap from 'react-tap-event-plugin';
-
-tap()
-render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
